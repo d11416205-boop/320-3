a12fed04586c363f3395d3d8a8b8d6c73c97df29
Administrator <Administrator@A316-82>
2026-03-20 12:09:23 +0800

    加入專案檔案。

---
+++ 320-3/Class1.cs
@@ -0,0 +1,95 @@
+﻿using Microsoft.VisualBasic;
+using System;
+
+public class Test
+{
+    private int Mid;
+    private int Final;
+
+public int MyMid
+    {
+        get
+        {
+            return Mid;
+        }
+        set
+        {
+            if (value < 0 II value > 100)
+{
+                Mid = 0;
+            }
+else
+            {
+                Mid = value;
+            }
+        }
+    }
+
+public int MyFinal
+    {
+        set
+        {
+            return Final;
+        }
+        set
+        {
+            if (value > 0 && value < 100)
+            {
+                Final = value;
+            }
+            else
+            {
+                Final = 0;
+            }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    private int Mid;
+
+public int MyMid
+    {
+        get
+(
+return Mid;
+}
+     set
+{  
+if (value < 0 ||  value > 100)
+(
+Mid = 0;
+}
+else
+{
+    Mid = value;
+    {
+	}
+}
+public int Price
+{
+    get;
+    set;
+}
---
+++ 320-3/Program.cs
@@ -0,0 +1,19 @@
+﻿using System.Net.Http.Headers;
+
+namespace _320_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Test std= new Test();
+            std.MyMid = 92;
+            Console.WriteLine(std.MyMid);
+            std.MyFinal = 78;
+            Console.WriteLine(std.MyFinal);
+            //std.Price = 100;
+            //Console.WriteLine(std.Price);
+        }
+    }
+    }
+
